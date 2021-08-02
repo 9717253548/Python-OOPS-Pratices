6f87adc442d3b2523ccb9e45bd76ec9ef9ba6bc3
9717253548 <suryavanshi2544@gmail.com>
2021-08-02 15:51:52 +0530

    first program

---
+++ progame1.py
@@ -0,0 +1,12 @@
+class Car():
+    def __init__(self,color,millage):
+        self.color = color
+        self.millage = millage
+
+class Description(Car):
+    def description(self):
+        print("this car color are ",self.color)
+
+d = Description('red',20)
+
+print(d.millage)
